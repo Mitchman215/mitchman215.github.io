885f411c638b13d517f8ae02dcc0937f9606395b
Mitchell Salomon <mitchell_salomon@brown.edu>
2022-06-02 23:15:45 -0400

    general and typography css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { render } from "solid-js/web"
 import App from "./components/App"
 import "./index.css"
+import "./css/styles.css"
 
 render(() => <App />, document.getElementById("root") as HTMLElement)

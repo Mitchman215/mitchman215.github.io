5d3d45696ac23a1de57488400303d81f454df65b
Mitchell Salomon <mitchell_salomon@brown.edu>
2022-06-04 00:46:05 -0400

    initial intro section styling

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -4,10 +4,11 @@ export default function Introduction() {
       <h1 class="section__title section__title--intro">
         Hi, I am <strong>Mitchell Salomon</strong>
       </h1>
-      <p class="section__subtitle section__subtitle--intro">full-stack dev</p>
+      <p class="section__subtitle section__subtitle--intro">full-stack developer</p>
       <img
         src="src/assets/mitchell-fancy-tux.jpeg"
         alt="Handsome Mitchell Salomon in a tuxedo"
+        class="intro__img"
       />
     </section>
   )

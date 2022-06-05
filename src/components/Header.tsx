f4bf1ddad50774e9cebdecbd73bdbb80adb6c86c
Mitchell Salomon <mitchell_salomon@brown.edu>
2022-06-05 16:30:16 -0400

    nav styling and functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,52 @@
 export default function Header() {
+  const logo = (
+    // Replace logo with svg of website name or smthg
+    <div class="logo">
+      <h1>Mitchell</h1>
+    </div>
+  )
+
+  function toggleNav() {
+    // console.log("clicked nav")
+    document.body.classList.toggle("nav-open")
+  }
+
   return (
     <header>
-      <div class="logo">
-        <h1>Mitchell Salomon</h1>
-      </div>
-
-      <button class="nav-toggle" aria-label="toggle navigation">
+      {logo}
+      <button class="nav-toggle" aria-label="toggle navigation" onClick={toggleNav}>
         <span class="hamburger"></span>
       </button>
 
       <nav class="nav">
         <ul class="nav__list">
-          <li class="nav__item">
-            <a href="#home" class="nav__link">
-              Home
-            </a>
-          </li>
-          <li class="nav__item">
-            <a href="#Interests" class="nav__link">
-              My Interests
-            </a>
-          </li>
-          <li class="nav__item">
-            <a href="#Bio" class="nav__link">
-              About me
-            </a>
-          </li>
-          <li class="nav__item">
-            <a href="#Portfolio" class="nav__link">
-              My Projects
-            </a>
-          </li>
+          <NavLink display="Home" link="home" />
+          <NavLink display="My Interests" link="interests" />
+          <NavLink display="About me" link="about" />
+          <NavLink display="My Projects" link="work" />
         </ul>
       </nav>
     </header>
+  )
+}
+
+interface NavLinkProps {
+  link: string
+  display: string
+}
+
+function NavLink(props: NavLinkProps) {
+  function onNavLinkClick(): void {
+    document.body.classList.remove("nav-open")
+  }
+
+  const link = `#${props.link}`
+
+  return (
+    <li class="nav__item">
+      <a href={link} class="nav__link" onClick={onNavLinkClick}>
+        {props.display}
+      </a>
+    </li>
   )
 }
